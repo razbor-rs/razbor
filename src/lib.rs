4a8a7500b5a04e0c3c0124675773d562ab0d26e5
Roman Proskuryakov <humbug@deeptown.org>
2019-12-21 00:52:46 +0300

    Move code into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use pest::iterators::Pair;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "m_expr.pest"]
+pub struct MexprParser;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Mexpr {
+    Apply { name: String, body: Vec<Mexpr> },
+    List(Vec<Mexpr>),
+    Name(String),
+    Number(String),
+    String(String),
+}
+
+impl Mexpr {
+    pub fn from_parsed(pair: Pair<Rule>) -> Self {
+        match pair.as_rule() {
+            Rule::mexpr => {
+                let list = pair
+                    .into_inner()
+                    .filter(|r| r.as_rule() != Rule::EOI)
+                    .map(Mexpr::from_parsed).collect();
+                Mexpr::List(list)
+            },
+            Rule::m => {
+                let mut inner = pair.into_inner();
+                let name = inner.next().unwrap().as_str().to_owned();
+                let body = inner.map(Mexpr::from_parsed).collect();
+
+                Mexpr::Apply { name, body }
+            },
+            Rule::list =>
+                Mexpr::List(
+                    pair.into_inner()
+                    .map(Mexpr::from_parsed)
+                    .collect()
+                ),
+            Rule::name =>
+                Mexpr::Name(pair.as_str().to_owned()),
+            Rule::number =>
+                Mexpr::Number(pair.as_str().to_owned()),
+            Rule::string =>
+                Mexpr::String(pair.into_inner().next().unwrap().as_str().to_owned()),
+            _ => panic!("{:?}", pair)
+        }
+
+    }
+}
