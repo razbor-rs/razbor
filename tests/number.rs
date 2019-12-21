390157498647c85642970df1187a26fdccb92104
Сухарик <65870+suhr@users.noreply.github.com>
2019-12-21 14:25:45 +0300

    Remove octal numbers

--- tests/number.rs
+++ tests/number.rs
@@ -23,27 +23,6 @@ fn decimal() {
 }
 
 #[test]
-fn octal() {
-    parses_to! {
-        parser: MexprParser,
-        input:  "00",
-        rule:   Rule::number,
-        tokens: [
-            octal(0, 2)
-        ]
-    };
-
-    parses_to! {
-        parser: MexprParser,
-        input:  "042",
-        rule:   Rule::number,
-        tokens: [
-            octal(0, 3)
-        ]
-    };
-}
-
-#[test]
 fn hexadecimal() {
     parses_to! {
         parser: MexprParser,
@@ -62,15 +41,15 @@ fn hexadecimal() {
 fn number() {
     parses_to! {
         parser: MexprParser,
-        input:  "[0, 1, 01, 12, 0x42]",
+        input:  "[0, 1, 12, 007, 0x42]",
         rule:   Rule::list,
         tokens: [
-            list(0, 20, [
+            list(0, 21, [
                 decimal(1, 2),
                 decimal(4, 5),
-                octal(7, 9),
-                decimal(11, 13),
-                hexdecimal(15, 19),
+                decimal(7, 9),
+                decimal(11, 14),
+                hexdecimal(16, 20),
             ])
         ]
     };
